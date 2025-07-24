829ee688210c8fefdcd76dfc4e412d2c098d97b9
Trinidad006 <trinidad.eps006@gmail.com>
2025-07-24 12:55:31 -0600

    feat: el login ahora devuelve el campo role junto con el token

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,13 +7,15 @@ export const login = async (req, res) => {
     const { heroId, password } = req.body;
     if (heroId === 1 && password === 'admin123') {
         // Login admin
-        const token = jwt.sign({ heroId: 1, role: 'admin' }, SECRET, { expiresIn: '1h' });
-        return res.json({ token });
+        const role = 'admin';
+        const token = jwt.sign({ heroId: 1, role }, SECRET, { expiresIn: '1h' });
+        return res.json({ token, role });
     }
     // Login usuario normal
     const heroes = await heroRepository.getHeroes();
     const heroExists = heroes.some(hero => hero.id === heroId);
     if (!heroExists) return res.status(401).json({ error: 'Superhéroe no encontrado' });
-    const token = jwt.sign({ heroId, role: 'user' }, SECRET, { expiresIn: '1h' });
-    res.json({ token });
+    const role = 'user';
+    const token = jwt.sign({ heroId, role }, SECRET, { expiresIn: '1h' });
+    res.json({ token, role });
 }; 
